ebd5708cbcf8a5140dfa42723f4e765e376a61e8
Ismail <Ismail@DESKTOP-G0GSDUM>
2022-04-13 11:32:46 +0200

    init

--- corporate.js
+++ corporate.js
@@ -1,13 +1,37 @@
-const ham=QuerySelector("ham");
+const hamburger=QuerySelector("hamburger");
 const meni=QuerySelector("meni");
 
 
-ham.addEventListener("click",() => {
-    ham.classList.toggle("active");
+hamburger.addEventListener("click",() => {
+    hambburger.classList.toggle("active");
     meni.classList.toggle("active");
-})                                                              /*listner ili liener*/
-document.querySelectorAll("meni-linkovi").forEach(n =>n.
+})                  
+                                            /*listner ili liener*/
+document.querySelectorAll("link").forEach(n =>n.
     addEventListener("click",()=>{
-        ham.classList.remove("active");
+        hamburger.classList.remove("active");
         meni.classList.remove("active");
     }))
+
+    function greska(){
+        var brisi=document.querySelector("body");
+        brisi.style.display="none";
+        
+    }
+    
+    do{
+            
+        if(querySelector("logo")){
+
+            style.width="149px";
+        }
+        else{
+
+            document.write("<h1>ok<h1>");
+        }
+       
+                            /*document.write(p + "<br>");
+                            p++;*/
+    
+    
+    }while(greska());
